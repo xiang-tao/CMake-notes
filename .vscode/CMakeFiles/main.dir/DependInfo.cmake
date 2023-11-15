db47cfcd385e3a9079753319f7a672995834b9dc
xiang-tao <1689415053@qq.com>
2023-11-15 19:43:55 +0800

    update

--- .vscode/CMakeFiles/main.dir/DependInfo.cmake
+++ .vscode/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/xt/github/CMake-notes/src/main.cpp" "/home/xt/github/CMake-notes/build/CMakeFiles/main.dir/src/main.cpp.o"
+  "/home/xt/github/CMake-notes/src/main.cpp" "/home/xt/github/CMake-notes/.vscode/CMakeFiles/main.dir/src/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
